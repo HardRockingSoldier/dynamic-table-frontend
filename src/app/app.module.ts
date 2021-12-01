aaae2529edff298d0394fd378e502d72409cb069
hardrockingsoldier <schliwa.s@gmail.com>
2021-12-01 14:07:39 +0100

    Added dynamic-table-configurator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DynamicTableConfiguratorComponent } from './dynamic-table-configurator/dynamic-table-configurator.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, BrowserAnimationsModule, MatTabsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, DynamicTableConfiguratorComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/dynamic-table-configurator/dynamic-table-configurator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-table-configurator',
+  templateUrl: './dynamic-table-configurator.component.html',
+  styleUrls: ['./dynamic-table-configurator.component.css']
+})
+export class DynamicTableConfiguratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
